a61f21c22b8eb78410482582098d0c683b733e93
Nicolas Le Flohic <leflohic.nicolas@gmail.com>
2020-07-21 17:05:34 +0200

    Adding global error handler

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRoutes = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 require('dotenv').config();
@@ -10,13 +13,26 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
 app.use(express.json());
-
 //serving local file
 app.use(express.static(`${__dirname}/public`));
 
+//ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRoutes);
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'Fail';
+  // err.statusCode = 404;
+  // next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+//Error handler
+app.use(globalErrorHandler);
 
 module.exports = app;
