c293f4657c5b87ad41daaf03ab924bde19c2d827
Nicolas Le Flohic <leflohic.nicolas@gmail.com>
2020-07-23 17:06:38 +0200

    Adding jwt in cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,8 +16,21 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   //log the user in send jwt
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ), //millisecond
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'prod') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  //remove password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
+    token,
     data: { user },
   });
 };
@@ -60,11 +73,12 @@ exports.signin = catchAsync(async (req, res, next) => {
   }
 
   //3 if everything ok, send token to the client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'Success',
-    token,
-  });
+  createSendToken(user, 200, res);
+  // const token = signToken(user._id);
+  // res.status(200).json({
+  //   status: 'Success',
+  //   token,
+  // });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -188,7 +202,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // => in the mongodb middleware
 
   //log the user in, send jwt
-  createSendToken(token, 200, res);
+  createSendToken(user, 200, res);
   // const token = signToken(user._id);
 
   // res.status(200).json({
