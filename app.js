ff5e7a2c58ba798b4bdd17bdfe3bf4cc92c62dba
Nicolas Le Flohic <leflohic.nicolas@gmail.com>
2020-07-31 14:10:42 +0200

    Implementing cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 //GLOBALS MIDDLEWARES
 //Set security HTTP Headers
+app.use(cors());
+//allow all routes
+app.options('*', cors());
+
 app.use('/api', helmet());
 
 //morgan logger
